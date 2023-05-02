6c02f63393a8d65c9a3bb03c37fe101118595765
Miffan20 <92362718+Miffan20@users.noreply.github.com>
2023-05-02 10:32:18 +0200

    added database connection

--- main.js
+++ main.js
@@ -8,3 +8,19 @@ app.set('view engine', 'ejs');
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port ${port}`);
 });
+
+
+//database connection
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "AdminAdmin123"
+});
+
+//checking if connection is completed
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
