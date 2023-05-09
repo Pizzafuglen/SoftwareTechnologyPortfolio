49999b3bfb99ba6630deeb6d1996e7397f410f3d
Chrisser-Mac <ChristofferRosenkilde@hotmail.com>
2023-05-09 19:48:04 +0200

    Small changes

--- public/resources/DataGathering.js
+++ public/resources/DataGathering.js
@@ -22,7 +22,7 @@ async function GetData() {
     const response = await fetch('https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15');
     const jsonData = await response.json();
 
-    console.log(jsonData[1]);
+    console.log(jsonData[1]['title']);
 }
 
 module.exports = {
