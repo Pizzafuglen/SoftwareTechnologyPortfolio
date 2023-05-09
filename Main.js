0911c0f318e0be30edb26c8fc75ff5b9d01f7208
Miffan20 <92362718+Miffan20@users.noreply.github.com>
2023-05-09 19:56:47 +0200

    videre med database connection
    har nu issues med at den ikke kan finde min database

--- Main.js
+++ Main.js
@@ -25,8 +25,10 @@ app.listen(port, () => {            //server starts listening for any attempts f
 
 
 //database connection
-var mysql2 = require('mysql');
+//var mysql2 = require('mysql');
 
+
+/*
 var con = mysql2.createConnection({
     host: "localhost",
     user: "root",
@@ -34,36 +36,41 @@ var con = mysql2.createConnection({
     databases: "steamgames"
 });
 
-//checking if connection is completed
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
 
-    fetch('https://www.cheapshark.com/api/1.0/deals?storeID=1&sortBy=savings&upperPrice=15')
+ */
 
+const fetch = require('node-fetch');
+const mysql = require('mysql');
 
-        .then(res => res.json())
-        //.then(data => console.log(data))
-        .then(data => {
-            const insertData = (deal) => {
-                const sql = 'INSERT INTO deals (title, salePrice, normalPrice, savings) VALUES (?, ?, ?, ?)';
-                const values = [deal.title, deal.salePrice, deal.normalPrice, deal.savings];
-                con.query(sql, values, (err, result) => {
-                    if (err) {
-                        console.error('Error inserting data into MySQL:', err);
-                        return;
-                    }
-                    console.log(`Inserted ${result.affectedRows} row(s)`);
-                });
-            };
-
-            data.forEach(deal => insertData(deal));
-        })
-        .catch(err => {
-            console.error('Error fetching data from API:', err);
-        });
+// create a connection to the MySQL database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'AdminAdmin123',
+    databases: 'steamgames'
 
 });
+
+// fetch data from the API
+fetch('https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15')
+    .then(response => response.json())
+    .then(data => {
+        // insert the data into the MySQL database
+        data.forEach(item => {
+            const sql = `INSERT INTO steamgames2 (title, salePrice, normalPrice, savings) VALUES (?, ?, ?, ?)`;
+            const values = [item.title, item.salePrice, item.normalPrice, item.savings];
+            connection.query(sql, values, (error, results, fields) => {
+                if (error) throw error;
+                console.log(`Inserted ${results.affectedRows} row(s)`);
+            });
+        });
+    })
+    .catch(error => console.error(error))
+    .finally(() => {
+        // close the MySQL connection when done
+        connection.end();
+    });
+
 
 
 
