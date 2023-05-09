193ef27f4f86abd4223d86c77fabf9354acc2309
Miffan20 <92362718+Miffan20@users.noreply.github.com>
2023-05-09 13:04:30 +0200

    added fetch, trying to add to database

--- Main.js
+++ Main.js
@@ -1,3 +1,5 @@
+
+
 const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 5001;                  //Save the port number where your server will be listening
@@ -9,14 +11,15 @@ app.listen(port, () => {            //server starts listening for any attempts f
     console.log(`Now listening on port ${port}`);
 });
 
-/*
+
 //database connection
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "AdminAdmin123"
+    password: "AdminAdmin123",
+    databases: "steamgames"
 });
 
 //checking if connection is completed
@@ -24,4 +27,48 @@ con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
 });
+
+
+const fetch = require('node-fetch');
+
+async function saveDataFromFetchCall(){
+
+    // make a fetch call for data
+    const response = await fetch ('https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15')
+    const data = await response.json();
+
+    //establish a connection to mySQL database
+
+    const connection = await mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "AdminAdmin123",
+        databases: "steamgames"
+    });
+
+    //define the SQL query o insert data:
+
+    const query = 'INSERT INTO steamgames2 (title, savings, salePrice, normalPrice) VALUES (?, ?, ?, ?)';
+
+    for (const game of data) {
+        const values = [game.title, game.savings, game.salePrice, game.normalPrice];
+        await connection.execute(query,values);
+    }
+
+    connection.end();
+}
+
+saveDataFromFetchCall();
+/*
+
+//This works:
+
+
+fetch('https://www.cheapshark.com/api/1.0/deals?storeID=1&sortBy=savings&upperPrice=15')
+
+
+    .then(res => res.json())
+    .then(data => console.log(data))
+
+
  */
