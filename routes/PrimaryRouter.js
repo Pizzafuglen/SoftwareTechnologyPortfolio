83131731818e348a2ae0f3669efa0feb05e77a2c
Chrisser-Mac <ChristofferRosenkilde@hotmail.com>
2023-05-09 10:23:37 +0200

    Small changes

--- routes/PrimaryRouter.js
+++ routes/PrimaryRouter.js
@@ -1,3 +1,8 @@
+/*
+The job of the PrimaryRouter is to ensure the that the different paths call the corresponding methods in the right
+controllers, leading the user to the correct views they are requesting.
+ */
+
 const express = require('express');
 const {indexView} = require('../controllers/IndexController');
 const {homeView} = require('../controllers/HomeController');
