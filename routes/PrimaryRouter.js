49ba7047fa4298ffb8a3f61b43710163a2d9727b
Chrisser-Mac <ChristofferRosenkilde@hotmail.com>
2023-05-09 10:20:40 +0200

    Small changes

--- routes/PrimaryRouter.js
+++ routes/PrimaryRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {indexView} = require('../controllers/IndexController');
 const {homeView} = require('../controllers/HomeController');
 const router = express.Router();
+
 router.get('/index', indexView);
 router.get('/',  homeView)
 module.exports = router;
