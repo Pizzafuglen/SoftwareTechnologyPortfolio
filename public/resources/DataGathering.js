a6511f8278c5364e6d99fa172b72d987d7adbd5f
Chrisser-Mac <ChristofferRosenkilde@hotmail.com>
2023-05-10 20:49:27 +0200

    Medium changes to the different files
    Primarily the PrimaryRouter.js file
    
    Next step: Implement the migration methods and then move on to requesting data from the db

--- public/resources/DataGathering.js
+++ public/resources/DataGathering.js
@@ -1,31 +1,148 @@
+const mysql = require('mysql2');
+const {login} = require("passport/lib/http/request");
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Chri42d5'
+});
+
+const tableName = 'gameOffers'
+
+
 /*
 The method so far calls the database, to ensure that there is a connection established
  */
 function ConnectToDB() {
-    const mysql = require('mysql2');
-
-    const con = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '***'
-    });
-
     con.connect(function(err) {
         if (err) throw err;
         console.log('Connected!');
     });
 }
+function CreateDB() {
+    con.connect(function(err) {
+        if (err) throw err;
+        con.query("CREATE DATABASE mydb", function (err, result) {
+            if (err) throw err;
+            console.log("Database created");
+        });
+    });
+}
+function CreateGameOfferTable() {
+    const con = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Chri42d5',
+        database: 'mydb'
+    });
+
+    con.connect(function(err) {
+        if (err) throw err;
+        const sql = 'CREATE TABLE gameOfferTable (name VARCHAR(255), address VARCHAR(255))';
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Table created");
+        });
+    });
+}
+
+function CreateStoreTable() {
+    const con = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Chri42d5',
+        database: 'mydb'
+    });
+
+    con.connect(function(err) {
+        if (err) throw err;
+        const sql = 'CREATE TABLE storeTable (name VARCHAR(255), address VARCHAR(255))';
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Table created");
+        });
+    });
+}
+
+function DropStoreTable() {
+    const con = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Chri42d5',
+        database: 'mydb'
+    });
+
+    con.connect(function(err) {
+        if (err) throw err;
+        const sql = 'DROP TABLE storeTable';
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Table deleted");
+        });
+    });
+}
+function DropGameOfferTable() {
+    const con = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Chri42d5',
+        database: 'mydb'
+    });
+
+    con.connect(function(err) {
+        if (err) throw err;
+        const sql = `DROP TABLE gameOfferTable`;
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Table deleted");
+        });
+    });
+}
+function ShowTables() {
+    const con = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Chri42d5',
+        database: 'mydb'
+    });
+
+    con.connect(function(err) {
+        if (err) throw err;
+        const sql = `SHOW TABLES`;
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log(result)
+        });
+    });
+}
+
+function MigrateStoreTable() {
+
+}
+function MigrateGameOfferTable() {
+
+}
+
 /*
 The method so far calls the API, and then posts the response to the log (right now it only post 'end')
  */
 async function GetData() {
-    const response = await fetch('https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15');
+    const response = await fetch('https://www.cheapshark.com/api/1.0/games?id=612');
     const jsonData = await response.json();
 
-    console.log(jsonData[1]['title']);
+    const dealsParsed = jsonData['deals'];
+    for (const item of dealsParsed) {
+        console.log(item['price'])
+    }
 }
 
 module.exports = {
     ConnectToDB,
-    GetData
+    GetData,
+    CreateDB,
+    CreateGameOfferTable,
+    DropStoreTable,
+    DropGameOfferTable,
+    CreateStoreTable,
+    ShowTables
 };
--- routes/PrimaryRouter.js
+++ routes/PrimaryRouter.js
@@ -4,10 +4,45 @@ controllers, leading the user to the correct views they are requesting.
  */
 
 const express = require('express');
+
 const {indexView} = require('../controllers/IndexController');
 const {homeView} = require('../controllers/HomeController');
+
+const{CreateDB} = require('../public/resources/DataGathering')
+const{CreateStoreTable} = require('../public/resources/DataGathering')
+const{CreateGameOfferTable} = require('../public/resources/DataGathering')
+const{DropGameOfferTable} = require('../public/resources/DataGathering')
+const{DropStoreTable} = require('../public/resources/DataGathering')
+const{ShowTables} = require('../public/resources/DataGathering')
+
+
 const router = express.Router();
 
 router.get('/index', indexView);
-router.get('/home', homeView)
+router.get('/home', homeView);
+router.get('/createStoreTable', function (req, res){
+    res.redirect('back')
+    CreateStoreTable()
+})
+router.get('/createGameOfferTable', function (req, res){
+    res.redirect('back')
+    CreateGameOfferTable()
+})
+router.get('/dropStoreTable', function (req, res) {
+    res.redirect('back')
+    DropStoreTable()
+})
+router.get('/dropGameOfferTable', function (req, res) {
+    res.redirect('back')
+    DropGameOfferTable()
+})
+router.get('/createDB', function (req, res){
+    res.redirect('back')
+    CreateDB()
+})
+router.get('/showTables', function (req, res){
+    res.redirect('back')
+    ShowTables()
+})
+
 module.exports = router;
